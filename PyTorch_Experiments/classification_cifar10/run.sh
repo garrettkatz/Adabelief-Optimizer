bbfd8c1e5baeb1c783515d17bc6675a7ade070f2
garrettkatz <garrett.katz@gmail.com>
2021-01-24 20:24:26 -0500

    longer cifar run

--- PyTorch_Experiments/classification_cifar10/run.sh
+++ PyTorch_Experiments/classification_cifar10/run.sh
@@ -19,4 +19,4 @@
 
 # trying more aggressive decay_epoch 60, total_epoch 110
 # resnet-abcapi-cl100-lr0.001-betas0.9-0.999-eps1e-08-wdecay0.0005-run0-resetFalse
-CUDA_VISIBLE_DEVICES=0 python3 main.py --classes 100 --model resnet --optim abcapi --lr 1e-3 --eps 1e-8 --beta1 0.9 --beta2 0.999 --momentum 0.9 --weight_decay 0.0005 --decay_epoch 60 --total_epoch 110
+CUDA_VISIBLE_DEVICES=0 python3 main.py --classes 100 --model resnet --optim abcapi --lr 1e-3 --eps 1e-8 --beta1 0.9 --beta2 0.999 --momentum 0.9 --weight_decay 0.0005 --decay_epoch 60 --total_epoch 200
